0b894ebf3d98ac0979e7e6971ffe0680353157fe
its-rajesh-smp <its.rajeshsmp@gmail.com>
2023-03-20 16:44:59 +0530

    Working Add Function 2.0

--- src/Components/AddNew/AddNew.jsx
+++ src/Components/AddNew/AddNew.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import "./AddNew.css";
-import {AddCard} from "../AddEditCard/AddEditCard";
 
-function AddNewBtn() {
+import {AddCard} from "../AddEditCard/AddCard";
 
-  const[setCard,setFunc]=useState(false)
+
+
+
+function AddNewBtn(props) {
+
+  const[setCard,setFunc]=useState(props.effect)
 
   function addClickHandaler(){
     setCard===false ? setFunc(true) : setFunc(true)
@@ -12,7 +16,7 @@ function AddNewBtn() {
 
   return(
     <>
-      {setCard && <AddCard />}
+      {setCard && <AddCard effect={setFunc} listHandeler={props.listHandeler}/> /*Passing  A Function To AddCard with the useState function when we execute this function it effect in this component */} 
       <div onClick={addClickHandaler} className="AddNew-div">+</div>;
     </>
     ) 
--- src/Components/Transaction/Transaction.js
+++ src/Components/Transaction/Transaction.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Transaction.css";
 import InputField from "../InputField/Input"
 import Item,{ItemCover} from "../Item/Item";
@@ -8,13 +8,19 @@ import AddNewBtn from "../AddNew/AddNew";
 
 function Transaction(){
 
+    // For TopMost Date In Transaction Window
     let date=new Date()
     let getDate=date.toLocaleDateString()
     
+
+    const [newList,setNewList]=useState([])
+
+
+
     return(
         <>
 
-            <AddNewBtn/>
+            <AddNewBtn effect={false} listHandeler={setNewList /*Passing This useState function to change the value in AddCard Component whenver someone click on Submit */ } />
 
             
             <div className="Heading-div">
@@ -34,30 +40,9 @@ function Transaction(){
 
 
             <ItemCover date="2022-03-0" >
-                <Item name="Sim Card" price="-6" date="2022-03-05" time="04:01:10" style={{color:"blue"}} />
-                <Item name="Mobile" price="-200" date="2022-03-05" time="07:06:10" />
+                {newList}
             </ItemCover>
 
-
-            <ItemCover date="2021-03-01">
-                <Item name="Food" price="-10" date="2021-03-01" time="22:50:00" />
-                <Item name="Back Cover" price="-3" date="2021-03-01" time="11:15:00" />
-            </ItemCover>
-
-
-            <ItemCover date="2020-09-07">
-                <Item name="Pizza" price="-20" date="2020-09-07" time="22:10:00" />
-                <Item name="Park" price="-15" date="2020-09-07" time="01:19:00" />
-            </ItemCover>
-
-
-            <ItemCover date="2019-04-06">
-                <Item name="Movie" price="-200" date="2019-04-06" time="20:58:00" />
-                <Item name="Pillow" price="-15" date="2019-04-06" time="02:01:00" />
-                <Item name="Potato" price="-3" date="2019-04-06" time="21:05:00" />
-                <Item name="Oil" price="-4" date="2019-04-06" time="22:19:00" />
-                <Item name="T-Shirt" price="-20" date="2019-04-06" time="24:15:00" />
-            </ItemCover>
         </>
     )
 }
